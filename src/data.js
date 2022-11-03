5e112f27938b4c20bebac3b8b3e4c5076666d293
sevrinbe <sevrin.eldred@gmail.com>
2022-11-03 15:20:37 -0500

    progress on portfolio, still working on it

---
+++ src/data.js
@@ -0,0 +1,52 @@
+import weatherAppScreenshot from "./assets/img/weatherappscreenshot.png";
+
+
+export const projects = [
+  {
+    title: "Weather App",
+    subtitle: "REST API Weather App",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+    link: {weatherAppScreenshot},
+  },
+  {
+    title: "Who's Playing??",
+    subtitle: "Find Events off a search.",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.gif",
+    link: "https://reedbarger.com",
+  },
+  {
+    title: "Daily Calendar",
+    subtitle: "Daily Calendar with data persistence.",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://jsbootcamp.com",
+  },
+  {
+    title: "Random Password Generator",
+    subtitle: "Simple generator",
+    description:
+      "A simple random password generator created with JavaScript. A series of prompts ask the user for password criteria to generate off of.",
+    image: "./project-4.gif",
+    link: "https://pythonbootcamp.com",
+  },
+];
+
+export const skills = [
+  "React",
+  "MySQL",
+  "MongoDB",
+  "GraphQL",
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "Git",
+  "Express",
+  "MERN",
+  "REST API",
+  "PWA",
+];
